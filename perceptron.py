bcc71de19c1d8a02b2b297f3f953682cd7073cc2
tommymcroskey <mcroskey.thomas@gmail.com>
2024-02-09 15:23:25 -0700

    implement fit function, comment breakdown

--- perceptron.py
+++ perceptron.py
@@ -17,7 +17,7 @@ class perceptron:
         """
         this constructor will default the value of iterations to 100,
         learning rate to .1, plot_data to False, random_w to False, and
-        seed to 10. Note that random_w in this case refers to the weight
+        seed to 10. Note that w in this case refers to the weight
         vector orthoganal to the hyperplane that perceptron will create
         """
 
@@ -28,26 +28,37 @@ class perceptron:
         self.seed = seed
 
     def fit(self, X, y):
-        if self.random_w :
-            rng = np.random.default_rng(self.seed)
-            self.w = rng.uniform(-1 , 1, len(X[0]))
+        """
+        This function serves to define the main converging loop,
+        where selectron will rotate a weight vector such that the
+        classification error is minimized over the training set.
+        """
+
+        if self.random_w:
+            rng = np.random.default_rng(self.seed)  # We use a seed for replicablility
+            self.w = rng.uniform(-1, 1, len(X[0]))  # Set weights to random values within [-1, 1]
             print("initialized with random weight vector")
-        else :
-            self.w = np.zeros(len(X[0]))
+        else:
+            self.w = np.zeros(len(X[0]))  # Initialize weights to zero
             print("initialized with a zeros weight vector")
-        self.wold = self.w
-        converged = False
-        iteration = 0
-        while (not converged and iteration <= self.iterations) :
-            converged = True
-            for i in range(len(X)) :
-                if y[i] * self.decision_function(X[i]) <= 0 :
-                    self.wold = self.w
-                    self.w = self.w + y[i] * self.learning_rate * X[i]
-                    converged = False
-                    if self.plot_data:
+
+        self.wold = self.w  # Store current weights to compare for convergence later
+        converged = False  # Flag to check if algorithm has converged
+        iteration = 0  # Counter to keep track of iterations
+
+        # Main loop to update weights until convergence or reaching max iterations
+        while (not converged and iteration <= self.iterations):
+            converged = True  # Assume convergence, prove otherwise
+            for i in range(len(X)):  # Iterate over all data points
+            # Update weights if classification is incorrect
+                if y[i] * self.decision_function(X[i]) <= 0:
+                    self.wold = self.w  # Keep track of old weights
+                    self.w = self.w + y[i] * self.learning_rate * X[i]  # Update weights
+                    converged = False  # Set converged to False as weights were updated
+                    if self.plot_data:  # If plotting is enabled, update the plot
                         self.plot_update(X, y, i)
-            iteration += 1
-        self.converged = converged
-        if converged :
-            print ('converged in %d iterations ' % iteration)
+            iteration += 1  # Increment iteration count
+
+        self.converged = converged  # Store convergence status
+        if converged:
+            print('converged in %d iterations ' % iteration)  # Print if converged within the iteration limit
