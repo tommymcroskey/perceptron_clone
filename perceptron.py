4d3dc1a01b388efa102879184783b2061e038900
tommymcroskey <mcroskey.thomas@gmail.com>
2024-02-09 11:17:14 -0700

    implement fit function

--- perceptron.py
+++ perceptron.py
@@ -28,4 +28,26 @@ class perceptron:
         self.seed = seed
 
     def fit(self, X, y):
-        
+        if self.random_w :
+            rng = np.random.default_rng(self.seed)
+            self.w = rng.uniform(-1 , 1, len(X[0]))
+            print("initialized with random weight vector")
+        else :
+            self.w = np.zeros(len(X[0]))
+            print("initialized with a zeros weight vector")
+        self.wold = self.w
+        converged = False
+        iteration = 0
+        while (not converged and iteration <= self.iterations) :
+            converged = True
+            for i in range(len(X)) :
+                if y[i] * self.decision_function(X[i]) <= 0 :
+                    self.wold = self.w
+                    self.w = self.w + y[i] * self.learning_rate * X[i]
+                    converged = False
+                    if self.plot_data:
+                        self.plot_update(X, y, i)
+            iteration += 1
+        self.converged = converged
+        if converged :
+            print ('converged in %d iterations ' % iteration)
